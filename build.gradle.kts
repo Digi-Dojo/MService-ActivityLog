45ca5a67e22e7cb93fd7c790e0838427ebddc1d4
Matteo Larcher <matteo.larcher.01@gmail.com>
2023-05-08 10:16:15 +0200

    Initializing the repo

---
+++ build.gradle.kts
@@ -0,0 +1,40 @@
+plugins {
+	java
+	id("org.springframework.boot") version "3.0.6"
+	id("io.spring.dependency-management") version "1.1.0"
+}
+
+group = "com.startupsdigidojo"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_17
+
+repositories {
+	mavenCentral()
+}
+
+extra["springCloudVersion"] = "2022.0.2"
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.apache.kafka:kafka-streams")
+	implementation("org.springframework.cloud:spring-cloud-stream")
+	implementation("org.springframework.cloud:spring-cloud-stream-binder-kafka")
+	implementation("org.springframework.cloud:spring-cloud-stream-binder-kafka-streams")
+	implementation("org.springframework.kafka:spring-kafka")
+	runtimeOnly("com.h2database:h2")
+	runtimeOnly("org.postgresql:postgresql")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.springframework.cloud:spring-cloud-stream-test-binder")
+	testImplementation("org.springframework.kafka:spring-kafka-test")
+}
+
+dependencyManagement {
+	imports {
+		mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
+	}
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
---
+++ src/main/java/com/startupsdigidojo/usersandteams/UsersandteamsApplication.java
@@ -0,0 +1,13 @@
+package com.startupsdigidojo.usersandteams;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsersandteamsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsersandteamsApplication.class, args);
+	}
+
+}
